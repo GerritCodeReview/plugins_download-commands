9f28bc052d274ed4173f5a08858ea16d845cf3c8
Edwin Kempin <edwin.kempin@sap.com>
2013-09-11 13:23:14 +0200

    Implement standard download schemes and commands
    Implement the standard download schemes and commands that are
    currently hard-coded in Gerrit code in a plugin.
    
    Change-Id: Icec3a8664f1fe4e18b0908ef47f39ba134890eba
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    Signed-off-by: David Pursehouse <david.pursehouse@sonymobile.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/download/command/RepoCommand.java
@@ -0,0 +1,48 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.download.command;
+
+import com.google.common.base.CaseFormat;
+import com.google.gerrit.extensions.config.DownloadCommand;
+import com.google.gerrit.extensions.config.DownloadScheme;
+import com.google.gerrit.server.config.DownloadConfig;
+import com.google.inject.Inject;
+
+import com.googlesource.gerrit.plugins.download.scheme.RepoScheme;
+
+public class RepoCommand extends DownloadCommand {
+  private final boolean commandAllowed;
+
+  @Inject
+  RepoCommand(DownloadConfig downloadConfig) {
+    this.commandAllowed = downloadConfig.getDownloadCommands().contains(
+        com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadCommand.REPO_DOWNLOAD);
+  }
+
+  @Override
+  public String getName() {
+    return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_HYPHEN,
+        com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadCommand.REPO_DOWNLOAD.name());
+  }
+
+  @Override
+  public String getCommand(DownloadScheme scheme, String project) {
+    if (!commandAllowed || !(scheme instanceof RepoScheme)) {
+      return null;
+    }
+
+    return scheme.getUrl(project) + " ${ref}";
+  }
+}
---
+++ src/main/java/com/googlesource/gerrit/plugins/download/scheme/AnonymousGitScheme.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.download.scheme;
+
+import com.google.gerrit.extensions.config.DownloadScheme;
+import com.google.gerrit.server.config.DownloadConfig;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.lib.Config;
+
+public class AnonymousGitScheme extends DownloadScheme {
+
+  private final String gitDaemonUrl;
+  private final boolean schemeAllowed;
+
+  @Inject
+  public AnonymousGitScheme(@GerritServerConfig Config cfg,
+      DownloadConfig downloadConfig) {
+    this.gitDaemonUrl =
+        ensureSlash(cfg.getString("gerrit", null, "canonicalGitUrl"));
+    this.schemeAllowed = downloadConfig.getDownloadSchemes().contains(
+        com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadScheme.ANON_GIT)
+        || downloadConfig.getDownloadSchemes().contains(
+        com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadScheme.DEFAULT_DOWNLOADS);
+  }
+
+  @Override
+  public String getName() {
+    return "Anonymous GIT";
+  }
+
+  @Override
+  public String getUrl(String project) {
+    StringBuilder r = new StringBuilder();
+    r.append(gitDaemonUrl);
+    r.append(project);
+    return r.toString();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return schemeAllowed && gitDaemonUrl != null;
+  }
+
+  private static String ensureSlash(String in) {
+    if (in != null && !in.endsWith("/")) {
+      return in + "/";
+    }
+    return in;
+  }
+}

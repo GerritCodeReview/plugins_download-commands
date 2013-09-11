e197222d44edf5c1113e42b51a4e43f0f70ea508
Edwin Kempin <edwin.kempin@sap.com>
2013-09-11 13:23:14 +0200

    Implement standard download schemes and commands
    Implement the standard download schemes and commands that are
    currently hard-coded in Gerrit code in a plugin.
    
    Change-Id: Icec3a8664f1fe4e18b0908ef47f39ba134890eba
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/download/Module.java
@@ -0,0 +1,50 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.download;
+
+import com.google.gerrit.extensions.config.DownloadCommand;
+import com.google.gerrit.extensions.config.DownloadScheme;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.inject.AbstractModule;
+
+import com.googlesource.gerrit.plugins.download.command.CheckoutCommand;
+import com.googlesource.gerrit.plugins.download.command.CherryPickCommand;
+import com.googlesource.gerrit.plugins.download.command.CloneCommand;
+import com.googlesource.gerrit.plugins.download.command.FormatPatchCommand;
+import com.googlesource.gerrit.plugins.download.command.PullCommand;
+import com.googlesource.gerrit.plugins.download.command.RepoCommand;
+import com.googlesource.gerrit.plugins.download.scheme.AnonymousGitScheme;
+import com.googlesource.gerrit.plugins.download.scheme.AnonymousHttpScheme;
+import com.googlesource.gerrit.plugins.download.scheme.HttpScheme;
+import com.googlesource.gerrit.plugins.download.scheme.RepoScheme;
+import com.googlesource.gerrit.plugins.download.scheme.SshScheme;
+
+class Module extends AbstractModule {
+  @Override
+  protected void configure() {
+    DynamicSet.bind(binder(), DownloadScheme.class).to(AnonymousHttpScheme.class);
+    DynamicSet.bind(binder(), DownloadScheme.class).to(AnonymousGitScheme.class);
+    DynamicSet.bind(binder(), DownloadScheme.class).to(HttpScheme.class);
+    DynamicSet.bind(binder(), DownloadScheme.class).to(RepoScheme.class);
+    DynamicSet.bind(binder(), DownloadScheme.class).to(SshScheme.class);
+
+    DynamicSet.bind(binder(), DownloadCommand.class).to(CloneCommand.class);
+    DynamicSet.bind(binder(), DownloadCommand.class).to(CheckoutCommand.class);
+    DynamicSet.bind(binder(), DownloadCommand.class).to(CherryPickCommand.class);
+    DynamicSet.bind(binder(), DownloadCommand.class).to(FormatPatchCommand.class);
+    DynamicSet.bind(binder(), DownloadCommand.class).to(PullCommand.class);
+    DynamicSet.bind(binder(), DownloadCommand.class).to(RepoCommand.class);
+  }
+}

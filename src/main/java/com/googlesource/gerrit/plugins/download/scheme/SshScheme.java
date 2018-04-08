f19a8d81cd3c8e7f797d35447d2cb9f15ce97bf1
David Ostrovsky <david@ostrovsky.org>
2018-04-08 19:25:24 +0200

    Adapt to recent changes in gerrit core
    Change-Id: If199b7923eba0daac6e8b6202da07918771b588c

--- src/main/java/com/googlesource/gerrit/plugins/download/scheme/SshScheme.java
+++ src/main/java/com/googlesource/gerrit/plugins/download/scheme/SshScheme.java
@@ -17,10 +17,10 @@ package com.googlesource.gerrit.plugins.download.scheme;
 import static com.google.gerrit.reviewdb.client.CoreDownloadSchemes.SSH;
 
 import com.google.gerrit.common.Nullable;
+import com.google.gerrit.config.CanonicalWebUrl;
+import com.google.gerrit.config.DownloadConfig;
 import com.google.gerrit.extensions.config.DownloadScheme;
 import com.google.gerrit.server.CurrentUser;
-import com.google.gerrit.server.config.CanonicalWebUrl;
-import com.google.gerrit.server.config.DownloadConfig;
 import com.google.gerrit.server.ssh.SshAdvertisedAddresses;
 import com.google.inject.Inject;
 import com.google.inject.Provider;

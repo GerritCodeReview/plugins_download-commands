bcfb05cbd1200422b81e9ac2ce824868ff74f252
David Pursehouse <dpursehouse@collab.net>
2019-02-28 16:48:01 +0900

    Format with google-java-format 1.7
    Change-Id: I4c053b01b8948c15aa2b3feb7790153b3ded9d4e

--- src/main/java/com/googlesource/gerrit/plugins/download/command/CloneWithCommitMsgHook.java
+++ src/main/java/com/googlesource/gerrit/plugins/download/command/CloneWithCommitMsgHook.java
@@ -78,7 +78,11 @@ public class CloneWithCommitMsgHook extends CloneCommand {
           .append(" ")
           .append(QuoteUtil.quote(projectName + "/.git/hooks/"));
       if (extraCommand != null) {
-        b.append(" && (cd ").append(QuoteUtil.quote(projectName)).append(" && ").append(extraCommand).append(")");
+        b.append(" && (cd ")
+            .append(QuoteUtil.quote(projectName))
+            .append(" && ")
+            .append(extraCommand)
+            .append(")");
       }
       return b.toString();
     }

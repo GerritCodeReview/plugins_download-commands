384a516c62fc32e4758da6f13f06c65b9625039e
David Pursehouse <dpursehouse@digital.ai>
2020-06-25 14:27:15 +0900

    New command: "Reset To"
    Add a new command that allows to reset the current branch to the commit
    that was fetched. This is useful in cases when the user is working on a
    local branch and the existing commands are not appropriate:
    
    - "Checkout" will check out the FETCH_HEAD, i.e. moving off the current
      branch and leaving the local repository in 'detached head' state.
    
    - "Cherry-Pick" will commit the fetched change again, resulting in a
      'new' commit (i.e. changed sha1) which will result in a new patch set
      if the user creates any commits on top of it and pushes for review.
    
    Change-Id: I683f88aa0d8ad6a6dd91f341bd23d363d329e174

---
+++ src/main/java/com/googlesource/gerrit/plugins/download/command/ResetCommand.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.download.command;
+
+import static com.google.gerrit.extensions.client.GeneralPreferencesInfo.DownloadCommand.CHECKOUT;
+
+import com.google.gerrit.server.config.DownloadConfig;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+import org.eclipse.jgit.lib.Config;
+
+public class ResetCommand extends GitDownloadCommand {
+  @Inject
+  ResetCommand(
+      @GerritServerConfig Config cfg,
+      DownloadConfig downloadConfig,
+      GitRepositoryManager repoManager) {
+    super(cfg, downloadConfig, CHECKOUT, repoManager);
+  }
+
+  @Override
+  String getCommand(String url, String ref, String id) {
+    return "git fetch " + QuoteUtil.quote(url) + " " + ref + " && git reset --hard FETCH_HEAD";
+  }
+}

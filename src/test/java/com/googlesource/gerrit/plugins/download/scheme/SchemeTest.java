dd4a2ed0ab53435cd4930a18dcb82d664564b9cc
Kaushik Lingarkar <kaushik.lingarkar@linaro.org>
2023-08-22 11:05:41 -0700

    SshScheme: Provide an option to omit usernames in SSH URLs
    Including the username in the download URL can prevent IT admins from
    configuring hosts to redirect read-only traffic to regional replicas
    when the admins rely on the git url.insteadOf configuration. This is
    because git doesn't support anything except simple replacement of
    matching strings, so there's no way to account for variable usernames in
    a host-wide /etc/gitconfig file.
    
    The default stays as always including the username in the SSH download
    commands.
    
    Release-Notes: Added an option to omit SSH usernames from download commands
    Change-Id: I49f3b85adc3dae2d440366c919fb656ca95b812f

--- src/test/java/com/googlesource/gerrit/plugins/download/scheme/SchemeTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/download/scheme/SchemeTest.java
@@ -15,11 +15,24 @@
 package com.googlesource.gerrit.plugins.download.scheme;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.server.config.DownloadConfig;
+import com.google.gerrit.server.config.PluginConfig;
+import com.google.gerrit.server.config.PluginConfigFactory;
 import com.googlesource.gerrit.plugins.download.DownloadCommandTest;
+import org.eclipse.jgit.lib.Config;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SchemeTest extends DownloadCommandTest {
+  @Mock
+  private PluginConfigFactory pluginConfigFactoryMock;
+
   @Test
   public void ensureHttpSchemeEncodedInUrl() throws Exception {
     assertThat(httpScheme.getUrl(ENV.projectName))
@@ -29,11 +42,33 @@ public class SchemeTest extends DownloadCommandTest {
   }
 
   @Test
-  public void ensureSshSchemeEncodedInUrl() throws Exception {
+  public void ensureSshSchemeEncodedInUrlWithUserName() throws Exception {
     assertThat(sshScheme.getUrl(ENV.projectName))
         .isEqualTo(
             String.format(
                 "ssh://%s@%s:%d/%s",
                 ENV.urlEncodedUserName(), ENV.fqdn, ENV.sshPort, ENV.projectName));
+  }
+
+  @Test
+  public void ensureSshSchemeNameEncodedInUrlWithoutUserName() {
+    assertThat(getSshSchemeUrlWithoutUserName())
+        .isEqualTo(String.format("ssh://%s:%d/%s", ENV.fqdn, ENV.sshPort, ENV.projectName));
+  }
+
+  private String getSshSchemeUrlWithoutUserName() {
+    PluginConfig.Update pluginConfig = PluginConfig.Update.forTest(PLUGIN_NAME, new Config());
+    pluginConfig.setBoolean("sshIncludeUserName", false);
+    when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME))
+        .thenReturn(pluginConfig.asPluginConfig());
+    SshScheme scheme =
+        new SshScheme(
+            ImmutableList.of(String.format("%s:%d", ENV.fqdn, ENV.sshPort)),
+            PLUGIN_NAME,
+            pluginConfigFactoryMock,
+            urlProvider,
+            userProvider,
+            new DownloadConfig(new Config()));
+    return scheme.getUrl(ENV.projectName);
   }
 }

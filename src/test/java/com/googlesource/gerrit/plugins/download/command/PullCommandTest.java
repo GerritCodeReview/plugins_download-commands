e38318facc226a2c824c5dbdbbcbd8fa73b70233
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-09-26 15:26:05 +0100

    Add `--recurse-submodules` flag to supporting commands
    When a repository contains git submodules, the simple `git checkout`
    command will leave the working tree with uncommited changes to the
    submodule references (aka dirty). This then requires a second git command
    to synchronize submodules.
    
    The checkout command already has the `--recurse-submodules` argument
    that will fetch submodules of checkout, ensuring that repository is in
    consistent state.
    
    Also using this flag with repositories that doesn't have submodules, the
    checkout will succeed without any complaint.
    
    The downside is that checkout command could issue multiple parallel
    fetch calls that may generate high load on the server. This is why to
    enable this feature, a modification to the plugin config is required.
    
    This change adds a new plugin configuraiton option:
    `download.recurseSubmodules`, by default set to `false`. That will add
    `--recurse-submodules` flag to the "checkout", "branch", "reset" and
    "pull" download commands.
    
    Bug: Issue 302090044
    Change-Id: I7b003dbd0687f173e8166a5ede931f773db5439c

---
+++ src/test/java/com/googlesource/gerrit/plugins/download/command/PullCommandTest.java
@@ -0,0 +1,63 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.download.command;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+
+import com.google.gerrit.server.config.DownloadConfig;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.googlesource.gerrit.plugins.download.DownloadCommandTest;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Test;
+
+public class PullCommandTest extends DownloadCommandTest {
+  private static final String TEST_URL = "unit.test/";
+  private static final String TEST_REF = "origin/main";
+  private static final String TEST_ID = "none";
+  private static final String PULL_COMMAND_SUFFIX = TEST_URL + " " + TEST_REF;
+
+  @Test
+  public void buildPullCommand() {
+    PullCommand pullCommand = newPullCommand(defaultGerritConfig());
+
+    String actual = pullCommand.getCommand(TEST_URL, TEST_REF, TEST_ID);
+
+    assertThat(actual).isEqualTo("git pull " + PULL_COMMAND_SUFFIX);
+  }
+
+  @Test
+  public void buildRecursivePull() {
+    Config cfg = defaultGerritConfig();
+    cfg.setBoolean("download", null, "recurseSubmodules", true);
+    PullCommand pullCommand = newPullCommand(cfg);
+
+    String actual = pullCommand.getCommand(TEST_URL, TEST_REF, TEST_ID);
+
+    assertThat(actual).isEqualTo("git pull --recurse-submodules " + PULL_COMMAND_SUFFIX);
+  }
+
+  private PullCommand newPullCommand(Config cfg) {
+    GitRepositoryManager gitRepositoryManagerMock = mock(GitRepositoryManager.class);
+
+    return new PullCommand(cfg, new DownloadConfig(cfg), gitRepositoryManagerMock);
+  }
+
+  private Config defaultGerritConfig() {
+    Config cfg = new Config();
+    cfg.setString("download", null, "command", "pull");
+
+    return cfg;
+  }
+}
